55237adce908efdcb504315234b2cf7cb0dc1ca7
BogyeumKim <namastae112@gmail.com>
2020-04-09 12:02:40 +0900

    ex

---
+++ java_project/Cp2-Valiable/src/ex/UseVariable.java
@@ -0,0 +1,15 @@
+package ex;
+
+public class UseVariable {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int num3;
+
+		num3 = num1 + num2;
+		System.out.print(num3);
+
+	}
+
+}
---
+++ java_project/FirstJava/src/chapter1/study0408a.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class study0408a {
+
+	public static void main(String[] args) {
+		
+		int i=15;
+		for(int a=0;a<5;a++) 
+				System.out.println(i);
+		
+	}
+
+}
