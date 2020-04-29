aac6e1cedb8077271d456fd58dc05fde633f8180
BogyeumKim <namastae112@gmail.com>
2020-04-29 18:30:13 +0900

    phonebookver5

---
+++ java_project/PhoneBook/src/ver05/exception/BadNumberException.java
@@ -0,0 +1,10 @@
+package ver05.exception;
+
+public class BadNumberException extends Exception {
+
+	public BadNumberException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ java_project/PhoneBook/src/ver05/exception/StringEmptyException.java
@@ -0,0 +1,10 @@
+package ver05.exception;
+
+public class StringEmptyException extends Exception {
+
+	public StringEmptyException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+}
